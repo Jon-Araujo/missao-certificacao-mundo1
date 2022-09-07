89f92164dc82228db8a26d17fc812bc700f39ceb
jamison0902 <jamison.queiroz@gmail.com>
2022-09-07 12:13:27 -0300

    Atualização na Reserva

--- crud_reserva.py
+++ crud_reserva.py
@@ -66,7 +66,7 @@ class AppBD:
 #----------------------------------------------------------------------------------------------------------------------
     def atualizarDados(self, codigo, ferramenta_id, descricao, data, horar, horad, tecnico_id):
         try:
-            if (self.verificaReserva(ferramenta_id, data, horar, horad)):
+            if (self.verificaReservaAtualizar(codigo, ferramenta_id, data, horar, horad)):
                 self.abrirConexao()
                 cursor = self.connection.cursor()
                 sql_update_query = "UPDATE reserva SET ferramenta_id=:ferramenta_id, descricao=:descricao, data=:data," \
@@ -117,7 +117,7 @@ class AppBD:
                 self.connection.close()
             return registros
 #----------------------------------------------------------------------------------------------------------------------
-# Selecionar tecnico
+# Seleciona Tecnico
 #----------------------------------------------------------------------------------------------------------------------
     def comboTecnico(self):
         try:
@@ -135,7 +135,7 @@ class AppBD:
                 self.connection.close()
             return registros
 #----------------------------------------------------------------------------------------------------------------------
-# Selecionar tecnico
+# Verifica Reserva
 #----------------------------------------------------------------------------------------------------------------------
     def verificaReserva(self, ferramenta_id, data, horar, horad):
         try:
@@ -167,4 +167,39 @@ class AppBD:
                 cursor.close()
                 self.connection.close()
             return registro
+#----------------------------------------------------------------------------------------------------------------------
+# Verifica Reserva
+#----------------------------------------------------------------------------------------------------------------------
+    def verificaReservaAtualizar(self, codigo, ferramenta_id, data, horar, horad):
+        try:
+            registro = False
+            self.abrirConexao()
+            cursor = self.connection.cursor()
+            sql_verifica_query = "SELECT count(*),horar,horad,id FROM reserva WHERE id!=:id AND ferramenta_id=:ferramenta_id " \
+                                 "AND data=:data AND horar<=:horad AND horad >= :horar"
+            cursor.execute(sql_verifica_query, {'id':codigo, 'ferramenta_id':ferramenta_id, 'data':data, 'horar':horar,
+                                                'horad':horad})
+            reg_verifica = cursor.fetchone()
+            #print(reg_verifica)
+            if (int(reg_verifica[0]) == 1):
+                messagebox.showinfo(title="Alerta", message="Ferramenta já reservada nesse período e data")
+            else:
+                query_time = "SELECT tempo FROM ferramenta WHERE descricao=:descricao"
+                cursor.execute(query_time, {'descricao':ferramenta_id})
+                reg_time = cursor.fetchone()
+                #print(reg_time)
+                if ((int(horad) - int(horar)) <= int(reg_time[0])):
+                    registro = True
+                else:
+                    messagebox.showinfo(title="Alerta", message=f"O período de utilização da ferramenta é de {reg_time[0]} horas")
+            self.connection.commit()
+        except (Exception, sqlite3.Error) as error:
+            print("Falha ao verificar reserva", error)
+        finally:
+            # closing database connection
+            if (self.connection):
+                cursor.close()
+                self.connection.close()
+            return registro
+
 
